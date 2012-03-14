8bd7fbe61e1fc1b3e27123cdcf63c18e546c02ea
casey <casey@ubuntu-dev-machine>
2012-03-14 18:31:02 -0400

    Moved some stuff around in new, created ContentMapper, and setup unit testing for ContentMapper.

---
+++ new/vendor/ContentMapper/Mapper.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace ContentMapper;
+
+class Mapper {
+
+  const FILEPATH = 1;
+  const URLPATH = 2;
+  
+  /**
+   * Content Folder
+   * @var string
+   */
+  private $content_path;
+	
+	// --------------------------------------------------------------	  
+  
+  /**
+   * @param string $content_folder 
+   */
+  public function __construct($content_path) {
+    
+    //Remove trailing slash if it exists
+    $content_path = rtrim($content_path, DIRECTORY_SEPARATOR);
+    
+    //Ensure path exists
+    if ( ! is_readable($content_path) OR ! is_dir($content_path)) {
+      throw new \RuntimeException("Cannot read from the content folder: $content_folder");
+    }
+        
+    $this->content_path = $content_path;
+  }
+	
+	// --------------------------------------------------------------	  
+  
+  /**
+   * Get a sitemap in array key/value format
+   * 
+   * Key is full URL & Value is the Title of the Item
+   * 
+   * @param string $subfolder 
+   */
+  public function get_sitemap($subdir = '') {
+    
+  }
+	
+	// --------------------------------------------------------------	
+  
+  /**
+   * Load a page object for a URL or filepath
+   * 
+   * @param string $path  URL or filepath of object
+   * @param int $type     Type of path sent (self::FILEPATH or self::URLPATH)
+   */
+  public function load_content_object($path, $type = self::URLPATH) {
+    
+    if ($type == self::URLPATH) {
+      $path = $this->map_url_to_filepath($path);
+    }
+    
+    return new Content_item($path);
+  }
+
+	// --------------------------------------------------------------	
+  
+  /**
+   * Map a URL path (e.g some/path) to a content item
+   * 
+   * @param string $path
+   * @return boolean|string  FALSE if the content does not exist, the realpath if it does
+   */
+  public function map_urlpath_to_filepath($urlpath) {
+    
+    $path = ltrim(str_replace('/', DIRECTORY_SEPARATOR, $path), DIRECTORY_SEPARATOR);
+    
+    if (file_exists($this->content_path . DIRECTORY_SEPARATOR . $path)) {
+      return $this->content_path . DIRECTORY_SEPARATOR . $path;
+    }
+    else {
+      return FALSE;
+    }
+      
+  }
+  
+	// --------------------------------------------------------------	
+   
+	/**
+	 * Recursive directory to scan pages and return objects 
+	 * 
+	 * @param string $path  If NULL, uses $this->content_dir
+	 */
+	private function scan_content_directory($path = NULL)
+	{
+		$pagelist = array();
+		
+		foreach(scandir($this->content_path . DIRECTORY_SEPARATOR . $path) as $file)
+		{
+			$filepath = $this->content_path . DIRECTORY_SEPARATOR . $path . DIRECTORY_SEPARATOR . $file;
+			
+			//We're only interested in directories
+			if ( ! is_dir($filepath))
+				continue;
+			
+			//Ignore hidden files
+			if ($file{0} == '.')
+				continue;
+			
+			//See if it's a page
+			try {
+				$item = $this->load_content_object($filepath, self::FILEPATH);
+			} catch (Exception $e) {
+				$item = FALSE;
+			}
+			
+			//If so, add it, and see what's underneath
+			if ($item)
+			{			
+				$pagelist[$item->path] = $item->title;
+				$pagelist = array_merge($pagelist, $this->scan_page_directory($filepath));
+			}
+		}
+		
+		return $pagelist;
+	}  
+  
+}
+
+/* EOF: Mapper.php */
---
+++ new/vendor/ContentMapper/tests/MapperTest.php
@@ -0,0 +1,99 @@
+<?php
+
+require_once(__DIR__ . '/../Mapper.php');
+
+class MapperTest extends PHPUnit_Framework_TestCase {
+
+  private $content_path;
+  
+  // --------------------------------------------------------------
+  
+  function setUp()
+  {
+    parent::setUp();
+    
+    $ds = DIRECTORY_SEPARATOR;
+    $this->content_path = sys_get_temp_dir() . $ds . 'phpunit_mapper_test_' . time();
+    
+    //Setup fake content directory
+    mkdir($this->content_path);
+    
+    //Fake front page
+    file_put_contents($this->content_path . $ds . 'content.php', "<p>Front Html</p>");
+    file_put_contents($this->content_path . $ds . 'meta.json', json_encode(array('title' => 'Front Page')));
+    
+    //Fake some_content page
+    mkdir($this->content_path . $ds . 'some_content');
+    file_put_contents($this->content_path . "{$ds}some_content{$ds}" . 'content.php', "<p>Some Content Html</p>");
+    file_put_contents($this->content_path . "{$ds}some_content{$ds}" . 'meta.json', json_encode(array('title' => 'Some Content')));
+
+    //Fake some_content/subcontent page
+    mkdir($this->content_path . $ds . 'some_content' . $ds . 'subcontent');
+    file_put_contents($this->content_path . "{$ds}some_content{$ds}subcontent{$ds}" . 'content.php', "<p>Subcontent Html</p>");
+    file_put_contents($this->content_path . "{$ds}some_content{$ds}subcontent{$ds}" . 'meta.json', json_encode(array('title' => 'Subcontent')));
+
+    //Fake some_other_content page
+    mkdir($this->content_path . $ds . 'some_other_content');
+    file_put_contents($this->content_path . "{$ds}some_other_content{$ds}" . 'content.php', "<p>Some Other Content Html</p>");
+    file_put_contents($this->content_path . "{$ds}some_other_content{$ds}" . 'meta.json', json_encode(array('title' => 'Some Other Content')));
+    
+    
+  }
+
+  // --------------------------------------------------------------
+
+  function tearDown()
+  {    
+    $ds = DIRECTORY_SEPARATOR;
+    
+    unlink($this->content_path . "{$ds}some_other_content{$ds}" . 'content.php');
+    unlink($this->content_path . "{$ds}some_other_content{$ds}" . 'meta.json');
+    rmdir($this->content_path . $ds . 'some_other_content');
+    
+    unlink($this->content_path . "{$ds}some_content{$ds}subcontent{$ds}" . 'content.php');
+    unlink($this->content_path . "{$ds}some_content{$ds}subcontent{$ds}" . 'meta.json');
+    rmdir($this->content_path . "{$ds}some_content{$ds}subcontent");
+    
+    unlink($this->content_path . "{$ds}some_content{$ds}" . 'content.php');
+    unlink($this->content_path . "{$ds}some_content{$ds}" . 'meta.json');
+    rmdir($this->content_path . $ds . 'some_content');
+    
+    unlink($this->content_path . $ds . 'content.php');
+    unlink($this->content_path . $ds . 'meta.json');
+    rmdir($this->content_path);
+    
+    parent::tearDown();
+  } 
+  
+  // --------------------------------------------------------------
+
+  public function testInstantiateAsObjectSucceeds() {
+    
+    $obj = new ContentMapper\Mapper($this->content_path);
+    $this->assertInstanceOf('ContentMapper\Mapper', $obj);
+  }
+  
+  // --------------------------------------------------------------
+
+  public function testTestDirectoriesAndFilesAreCorrectlySetup() {
+    
+    $ds = DIRECTORY_SEPARATOR;
+    
+    $this->assertFileExists($this->content_path . $ds . 'content.php');
+    $this->assertFileExists($this->content_path . $ds . 'meta.json');
+    $this->assertFileExists($this->content_path . $ds . 'some_content' . $ds . 'content.php');
+    $this->assertFileExists($this->content_path . $ds . 'some_content' . $ds . 'meta.json');
+    $this->assertFileExists($this->content_path . $ds . 'some_content' . $ds . 'subcontent' . $ds . 'content.php');
+    $this->assertFileExists($this->content_path . $ds . 'some_content' . $ds . 'subcontent' . $ds . 'meta.json');
+    $this->assertFileExists($this->content_path . $ds . 'some_other_content' . $ds . 'content.php');
+    $this->assertFileExists($this->content_path . $ds . 'some_other_content' . $ds . 'meta.json');    
+  }
+  
+  // --------------------------------------------------------------
+
+  //LEFT OFF HERE LEFT OFF HERE LEFT OFF HERE!
+  //Need to test the actual methods in the file...
+
+}
+
+/* EOF: MapperTest.php */
