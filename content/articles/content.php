8411b0cab8f0f7fa2033b5d5ce4875d139fad81e
casey <casey@ubuntu-dev-machine>
2012-04-10 15:21:10 -0400

    Updated content and all kinds of stuff.  Currently working on Article List.

--- content/articles/content.php
+++ content/articles/content.php
@@ -1,1 +1,85 @@
-<p>Articles</p>
+<h1>Articles</h1>
+
+<p>
+  @TODO: Finish This 
+  Refer to http://192.168.5.5/casey-sandbox/projects/caseyamcl.com/greenandbrown/?page=articles_short_list
+</p>
+
+<?php 
+
+  //Sort posts descending by date
+  uasort($page_children, function($a, $b) {
+    if ( ! isset($a->meta->date_published)) {
+      return -1;
+    }
+    elseif ( ! isset($b->meta->date_published)) {
+      return 1;
+    }
+    else {
+      $a_year = substr($a->meta->date_published, 0, 4);
+      $b_year = substr($b->meta->date_published, 0, 4);
+      
+      if ($a_year == $b_year) {
+        return 0;
+      }
+      else {
+        return ($a_year > $b_year) ? -1 : 1;
+      }
+    }
+  });
+  
+?>
+
+<?php if (count($page_children) > 0): ?>
+<div class="article_list">
+  <?php 
+    $other = array();
+    foreach($page_children as $url => $child): 
+  ?>
+  
+  <?php
+  
+    //Print the year header
+    if ( ! isset($child->meta->date_published)) {
+      $other[$url] = $child;
+      continue;
+    }
+    elseif ($child->meta->date_published) {
+      
+      $year = substr($child->meta->date_published, 0, 4);
+      
+      if ( ! isset($curr_year)) {
+        $curr_year = $year;
+        echo "<h4>$year</h4>\n";
+        echo "<ul>";
+      }
+      elseif ($year != $curr_year) {
+        $curr_year = $year;
+        echo "</ul>";
+        echo "<h4>$year</h4>\n";
+        echo "<ul>\n";
+      }
+    }
+    
+    //Print all of the list items:
+  ?>
+  
+  <li>
+    <a href="<?php echo $url; ?>">
+      <strong>Mon ## -</strong>
+      <span class="title"><?php echo $child->title; ?></span>
+      <span class="summary"><?php echo $child->meta->summary; ?></span>
+    </a>
+  </li>
+  
+  <?php
+    echo "</ul>";
+    endforeach;
+  ?>
+</div>
+
+<?php else: ?>
+
+<p>Hmm. There don't seem to be any articles here.  Odd.</p>
+
+<?php endif; ?>
--- content/code/content.php
+++ content/code/content.php
@@ -1,24 +1,84 @@
-<div class="two-thirds column">
-	<h1>Code</h1>
-	
-	<p>
-		I write webapps, and mostly work with PHP, Drupal, and CodeIgniter.  Below
-		are some of my open source projects:
-	</p>
-	
-	<ul>
-		<li>List projects here (Manually code Github URL for select projects Curl->get()...)</li>
-	</ul>
-	
+<h1>Code and Other Resources</h1>
+
+<div class="section_intro">
+  
+  <div class="section_intro_blurb">
+    <h2>Open Source Code</h2>
+    <p>
+      I write code, mostly in PHP.  I have worked extensively with Drupal,
+      CodeIgniter, Jquery, and other fun web technologies.
+    </p>
+  </div>
+
+  <div class="section_auxiliary_info">
+    <h3>Find Me On&hellip;</h3>
+    <ul>
+      <li>
+        <a href="https://github.com/caseyamcl" title="Github">
+          <img src="<?php echo $page_url; ?>github_logo.png" alt="Github" />
+        </a>
+      </li>
+      <li>
+        <a href="https://bitbucket.org/caseyamcl" title="Bitbucket">
+          <img src="<?php echo $page_url; ?>bitbucket_logo.png" alt="Bitbucket" />
+        </a>
+      </li>
+      <li>
+        <a href="http://stackoverflow.com/users/143201/caseyamcl" title="StackOverflow">
+          <img src="<?php echo $page_url; ?>stackexchange_logo.png" alt="Stack Overflow" />
+        </a>
+      </li>
+      <li>
+        <a href="http://drupal.org/user/1545572" title="Drupal.org">
+          <img src="<?php echo $page_url; ?>drupal_logo.png" alt="Drupal.org" />
+        </a>
+      </li>
+    </ul>
+  </div>
 </div>
 
-<div class="one-third column">
-	<h5>I'm Also On</h5>
-	
-	<ul>
-		<li><a href="http://stackoverflow.com/users/143201/caseyamcl">StackOverflow.com</a></li>
-		<li><a href="http://github.com/caseyamcl">Github</a></li>
-	</ul>
-</div>
+<hr />
 
+<ul class="tile_list">
+  
+  <li>
+    <h5>GeSHi for CodeIgniter</h5>
+    <img src="http://dummyimage.com/80x80/999/fff" alt="Dummy Img">
+    <p>
+      This is GeSHi for CodeIgniter.  It is a simple tool that integrates
+      GeSHi Syntax Highlighting with CodeIgniter.
+      <a href="http://github.com/caseyamcl/" title="Github Page" class="more_link">More (on Github)&hellip;</a>
+    </p>
+  </li>
+
+  <li>
+    <h5>User Tracking for CodeIgniter</h5>
+    <img src="http://dummyimage.com/80x80/999/fff" alt="Dummy Img">
+    <p>
+      A library for CodeIgniter that tracks user activity on a site and associates it
+      with a User ID
+      <a href="http://github.com/caseyamcl/" title="Github Page" class="more_link">More (on Github)&hellip;</a>
+    </p>
+  </li>
+  
+  <li>
+    <h5>GeSHi for CodeIgniter</h5>
+    <img src="http://dummyimage.com/80x80/999/fff" alt="Dummy Img">
+    <p>
+      This is GeSHi for CodeIgniter.  It is a simple tool that integrates
+      GeSHi Syntax Highlighting with CodeIgniter.
+      <a href="http://github.com/caseyamcl/" title="Github Page" class="more_link">More (on Github)&hellip;</a>
+    </p>
+  </li>
+  
+  <li>
+    <h5>GeSHi for CodeIgniter</h5>
+    <img src="http://dummyimage.com/80x80/999/fff" alt="Dummy Img">
+    <p>
+      This is GeSHi for CodeIgniter.  It is a simple tool that integrates
+      GeSHi Syntax Highlighting with CodeIgniter.
+      <a href="http://github.com/caseyamcl/" title="Github Page" class="more_link">More (on Github)&hellip;</a>
+    </p>
+  </li>  
+</ul>
 
