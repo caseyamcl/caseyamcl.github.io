1387858905f93b42bf343cab2a695069f31a55a5
Casey McLaughlin <caseyamcl@gmail.com>
2012-06-05 17:34:49 -0400

    Reworked the site style

--- template/template.php
+++ template/template.php
@@ -7,15 +7,18 @@
   
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
   
-  <?php echo load_less_css($template_path, $template_url); ?>
+  <link rel="stylesheet" type="text/css" href="<?php echo $template_url; ?>css/01-reset.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="<?php echo $template_url; ?>css/02-base.css"  media="all" />
+  <link rel="stylesheet" type="text/css" href="<?php echo $template_url; ?>css/03-typography.css" media="all"  />
+  <link rel="stylesheet" type="text/css" href="<?php echo $template_url; ?>css/04-layout.css"  media="all" />
+  <link rel="stylesheet" type="text/css" href="<?php echo $template_url; ?>css/05-print.css"  media="print" />
 </head>
 
 <body>
   
   <header>
-      <h1>
-        <a href="<?php echo $site_url; ?>">Casey McLaughlin</a>
-      </h1>
+      <h1><a href="<?php echo $site_url; ?>">Casey McLaughlin</a></h1>
+      <h2>IT, Development, Systems Administration, and Life in Tallahassee, Florida</h2>
       <nav role="navigation">
         
         <?php        
