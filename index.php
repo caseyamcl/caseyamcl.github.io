bfcb7c548ba312730ced83dae78469ec3ed3cf38
Casey McLaughlin <caseyamcl@gmail.com>
2013-01-11 14:55:49 -0500

    Moved folder structure around to be AppFog-friendly

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file index.php
+ * CaseyMcLaughlin.com Public Index File
+ *
+ * @package CaseyMcLaughlin.com
+ * @author Casey McLaughlin
+ */
+
+//Autoloader
+require(__DIR__ . '/app/vendor/autoload.php');
+
+
+//Away we go
+\Caseyamcl\App::main();
+
+/* EOF: index.php */
