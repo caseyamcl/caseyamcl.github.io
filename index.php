a1a04721283cecebc8731c3b9a332f1d0f8ce7ab
Casey McLaughlin <caseyamcl@gmail.com>
2013-02-20 16:11:50 -0500

    Minor changes, friendly composer error message, and updated dependencies removing QueryPath

--- index.php
+++ index.php
@@ -8,6 +8,13 @@
  * @author Casey McLaughlin
  */
 
+//Sanity check
+if ( ! is_readable(__DIR__ . '/app/vendor/autoload.php')) {
+    header("HTTP/1.1 500 Internal Server Error");
+    header("Content-type: text/plain");
+    die("CaseyMcLaughlin.com is not installed correctly.  Check back later.");
+}
+
 //Autoloader
 require(__DIR__ . '/app/vendor/autoload.php');
 
